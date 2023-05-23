6fbbfe70e725c99dde6321d43de8a4976876dc19
Aneesha S <aneesha21110377@SNUCHENNAI.EDU.IN>
2023-05-23 17:24:32 +0530

    placementdrive DAL,BOL

---
+++ PMS.BOL/Models/PlacementDriveDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PMS.BOL
+{
+    public class PlacementDriveDto 
+    {
+        public int PlacementDriveId { get; set; }
+
+        public int CollegeId { get; set; }
+
+        public int CompanyId { get; set; }
+
+        public DateTime InterviewDate { get; set; }
+
+        public int NoOfVacancies { get; set; }
+
+        public string Package { get; set; }
+
+        public string Place { get; set; }
+
+        public string Title { get; set; }
+
+        public string Details { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public byte Status { get; set; }
+        public CollegeDto CollegeDto { get; set; }
+        public CompanyDto CompanyDto { get; set; }
+
+    }
+
+}

4796e42993df33eb0735b922004df29d00defd9e
Aneesha S <aneesha21110377@SNUCHENNAI.EDU.IN>
2023-05-23 22:36:46 +0530

    Controller-incomplete

---
+++ PMS/Controllers/PlacementDriveController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PMS.BLL;
+using.BOL;
+using PMS.BOL;
+
+namespace PMS.Controllers
+{
+    public class PlacementDriveController : Controller
+    {
+        IPlacementDriveBLL _placementDriveBll;
+       
+        public PlacementDriveController(IPlacementDriveBLL placementDriveBll)
+        {
+            _placementDriveBll = placementDriveBll;
+        }
+
+        public IActionResult Index()
+        {
+            var placementdriveList=_placementDriveBll.GetAllPlacementDrivebll();
+            return View(placementdriveList);
+        }
+        public IActionResult Edit(int id)
+        {
+            ViewBag.ErrorCnfMsg = null;
+            var placementdrive = _placementDriveBll.GetPlacementDriveByPlacementDriveId(id);
+            return View(placementdrive);
+        }
+        [HttpPost]
+       
+
+}
+   
